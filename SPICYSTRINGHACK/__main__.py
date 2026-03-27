8d7a4616e20f9155f9581d5f6b96e0701c664157
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-27 16:00:09 +0530

    Update __main__.py

--- SPICYSTRINGHACK/__main__.py
+++ SPICYSTRINGHACK/__main__.py
@@ -8,7 +8,7 @@ from SPICYSTRINGHACK.modules import ALL_MODULES
 
 async def start_bot():
     for all_module in ALL_MODULES:
-        importlib.import_module("DAXXSTRINGHACK.modules." + all_module)
+        importlib.import_module("SPICYSTRINGHACK.modules." + all_module)
     LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
     await idle() 
     LOG.print("[bold red]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
