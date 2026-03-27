5d129d68751c8bb4413e66f72750379ed8574034
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-27 16:20:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config:
     API_HASH = "f4871ddfefa0b6c25047f29e573e0a0d"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", "")
-    MONGO_URL = "mongodb+srv://STRINGHACK:Nothing0000@cluster0.nbjdl3k.mongodb.net/?appName=Cluster0"
+    MONGO_URL = ""
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["8438947274","924235973"])
